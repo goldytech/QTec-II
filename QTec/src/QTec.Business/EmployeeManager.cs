11dee4df9b20d2b4090fb37005fdaed97d3a8b66
Muhammad Afzal <afzal.qureshi@outlook.com>
2014-04-29 18:56:19 -0700

    Added mising files

---
+++ QTec/src/QTec.Business/EmployeeManager.cs
@@ -0,0 +1,74 @@
+﻿namespace QTec.Business
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using QTec.Core.Model;
+    using QTec.Data;
+
+    /// <summary>
+    /// The employee manager.
+    /// </summary>
+    public class EmployeeManager : IEmployeeManager
+    {
+        /// <summary>
+        /// The QTec unit of work.
+        /// </summary>
+        private readonly IQTecUnitOfWork qTecUnitOfWork;
+
+       /// <summary>
+        /// Initializes a new instance of the <see cref="EmployeeManager"/> class.
+        /// </summary>
+        /// <param name="qTecUnitOfWork">
+        /// The QTec unit of work.
+        /// </param>
+        public EmployeeManager(IQTecUnitOfWork qTecUnitOfWork)
+        {
+            if (qTecUnitOfWork == null)
+            {
+                throw new ArgumentNullException("qTecUnitOfWork");
+            }
+
+            this.qTecUnitOfWork = qTecUnitOfWork;
+        }
+
+       /// <summary>
+        /// The add employee.
+        /// </summary>
+        /// <param name="employee">
+        /// The employee.
+        /// </param>
+        public async void AddEmployee(Employee employee)
+        {
+           await this.qTecUnitOfWork.EmployeeRepository.Insert(employee);
+        }
+
+        /// <summary>
+        /// The get employee by id.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Employee"/>.
+        /// </returns>
+        public async Task<Employee> GetEmployeeById(int id)
+        {
+            var employee = await this.qTecUnitOfWork.EmployeeRepository.GetByIdAsync(id);
+            return employee;
+        }
+
+        /// <summary>
+        /// The get employees.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            var employees = await this.qTecUnitOfWork.EmployeeRepository.RetrieveAllRecordsAsync();
+            return employees;
+        }
+    }
+}
---
+++ QTec/src/QTec.Business/IEmployeeManager.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IEmployeeManager.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The EmployeeManager interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QTec.Business
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using QTec.Core.Model;
+
+    /// <summary>
+    /// The EmployeeManager interface.
+    /// </summary>
+    public interface IEmployeeManager
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+       
+        /// <summary>
+        /// The add employee.
+        /// </summary>
+        /// <param name="employee">
+        /// The employee.
+        /// </param>
+        void AddEmployee(Employee employee);
+    }
+}
