b75e6d9b513a068622771baa7d71325b39eed13d
Muhammad Afzal <afzal.qureshi@outlook.com>
2014-04-29 18:49:51 -0700

    Added employee view and controller

---
+++ QTec/src/QTec.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QTec.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using QTec.Business;
+
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeManager employeeManager;
+
+        public EmployeeController(IEmployeeManager employeeManager)
+        {
+            this.employeeManager = employeeManager;
+        }
+
+        //
+        // GET: /Employee/
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public async Task<ViewResult> Index()
+        {
+            var employees = await this.employeeManager.GetEmployees();
+            return this.View(employees);
+        }
+
+        //
+        // GET: /Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employee/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
